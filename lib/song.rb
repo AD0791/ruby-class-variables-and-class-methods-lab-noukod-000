5c0f45ab2d54522644df3e726866a2bae6005eea
Alexandro Disla <alexandrodisla@hotmail.com>
2019-05-06 19:56:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,9 +6,9 @@ class Song
   attr_accessor :name, :artist, :genre
   
   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
+    self.name = name
+    self.artist = artist
+    self.genre = genre
     @@count+=1
   end
   
